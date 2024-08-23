d9dc1913e8dd9f3e56528f76c66df85570107029
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-08-22 22:48:12 -0300

    Projeto aplicativo lista de tarefas inicio

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  useEffect(()=>{
+  console.warn("checked!")
+},[]);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{color:'red'}}>Barbara é linda! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Tasks/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.TouchableOpacity`
+    width: 100%;
+    heigth: 56;
+    flex-direction: row;
+    justify-content: space-between;
+    align-itens: center;
+    background-color: #304163;
+    border-radius: 4px;
+`;
