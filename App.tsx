d5d4a707004cfecf0a0054892fdb466b5f2374c8
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-08-23 17:25:16 -0300

    Incluido funcoes de usestate no projeto

--- App.tsx
+++ App.tsx
@@ -1,33 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { Tasks } from './src/components/Tasks';
 import { CardNumber } from './src/components/CardNumber';
 import { InputAddTask } from './src/components/InputAddTask';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
 
-  useEffect(()=>{
-  console.warn("checked!")
-},[]);
-
-
+  const [Task, setTask] = useState<{description: string; checked: boolean}[]>([]);
+  const [taskText, setTaskText] = useState("");
 
   return (
     <View style={styles.container}>
+      
       <InputAddTask/>
       <View style={{flexDirection: 'row', gap: 16}}>
         <CardNumber />
         <CardNumber />
         <CardNumber />
         </View>    
-      <Tasks />
-      <Tasks />
-      <Tasks />
-      <Tasks />
-      <Tasks />
+  
+      
+    <FlatList
+
+      data={Task}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={
+        ({item}) => (
+          <Tasks />
+        )
+      }
+
+      ListEmptyComponent={() => (
+
+        <View><Text >Você ainda não adcionou nenhuma task!</Text></View>
+      )}
+
+
+        />
+
       <StatusBar style="auto" />
     </View>
+
+    
+
+
   );
 }
 
@@ -38,6 +56,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 16,
+    paddingTop: 64,
     gap: 16,
   },
 });
