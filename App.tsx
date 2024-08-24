e35738feded8f8a8cd010ae5413f89dc2994950e
Alexandre Amaral <alexandreamaral.gt@gmail.com>
2024-08-24 19:52:13 -0300

    Criado funcao useState no projeto, incluido verificaçao se o campo de texto esta vazio, verificacao se a tarefa ja existe

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Tasks } from './src/components/Tasks';
 import { CardNumber } from './src/components/CardNumber';
 import { InputAddTask } from './src/components/InputAddTask';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Feather }  from '@expo/vector-icons';
 
 export default function App() {
 
-  const [Task, setTask] = useState<{description: string; checked: boolean}[]>([]);
+  const [tasks, setTask] = useState<{description: string; checked: boolean}[]>([]);
   const [taskText, setTaskText] = useState("");
+
+  function handleTaskAdd(){
+   
+    if(taskText.length > 0){
+      console.log("Vazio!");
+    }
+
+    if(tasks.some((tasks)=> tasks.description === taskText)) {
+    console.log("Essa tarefa já existe!");
+    }
+
+    const newTask = {description: taskText, checked: false};
+    setTask([...tasks, newTask]);
+    setTaskText('');
+  }
 
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       
-      <InputAddTask/>
+      <View style={styles.InputContainer}>
+        <TextInput
+        style={styles.input}
+        placeholder='Digite a tardefa...'
+        placeholderTextColor="white"
+        keyboardType='default' 
+        onChangeText={setTaskText}
+        value={taskText}   
+        />
+        <TouchableOpacity style={styles.InputButton}>
+        <Feather name='send' size={24} color="white" />
+        </TouchableOpacity>
+        </View>
+
       <View style={{flexDirection: 'row', gap: 16}}>
         <CardNumber />
         <CardNumber />
@@ -24,7 +54,7 @@ export default function App() {
       
     <FlatList
 
-      data={Task}
+      data={tasks}
       keyExtractor={(item, index) => index.toString()}
       renderItem={
         ({item}) => (
@@ -35,17 +65,8 @@ export default function App() {
       ListEmptyComponent={() => (
 
         <View><Text >Você ainda não adcionou nenhuma task!</Text></View>
-      )}
-
-
-        />
-
-      <StatusBar style="auto" />
+      )}/>
     </View>
-
-    
-
-
   );
 }
 
@@ -59,4 +80,30 @@ const styles = StyleSheet.create({
     paddingTop: 64,
     gap: 16,
   },
+
+  InputContainer:{
+
+    flexDirection: 'row',
+    borderRadius: 4,
+    backgroundColor: '#252627',
+
+  },
+
+  input: {
+
+    flex: 1,
+    padding: 16,
+    color: '#fff',
+
+  },
+
+  InputButton: {
+
+    backgroundColor: '#1E1E1E',
+    padding: 16,
+    borderRadius: 4,
+
+  }
+
 });
+
